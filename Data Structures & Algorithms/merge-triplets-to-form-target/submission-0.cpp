007e4a1a5387d2a5d9073f25d8ce235dcb708fa9
NAINCY RAUNIYAR <144170524+NaincyRauniyar@users.noreply.github.com>
2026-07-06 23:47:06 +0530

    Add: merge-triplets-to-form-target - submission-0

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        int n = triplets.size();
+        vector<int> res= {0,0,0};
+        for(int i=0;i<n;i++){
+            if(triplets[i][0]<= target[0] &&
+                triplets[i][1] <= target[1]&&
+                triplets[i][2] <= target[2]){
+            res[0]= max(res[0],triplets[i][0]);
+            res[1]= max(res[1],triplets[i][1]);
+            res[2]= max(res[2],triplets[i][2]);
+        }
+        if(res==target) return true;
+    }
+        return false;
+    }
+};
