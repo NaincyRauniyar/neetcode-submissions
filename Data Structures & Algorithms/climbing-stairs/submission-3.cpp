b16188ca06617db2bfb76339adc76bea336d8630
NAINCY RAUNIYAR <144170524+NaincyRauniyar@users.noreply.github.com>
2026-07-26 11:42:13 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+// int f(int n,vector<int> &dp){
+//   if(n<=1) return 1;
+//   if(dp[n] != -1) return dp[n];
+//   int one = f(n-1,dp);
+//   int two = f(n-2,dp);
+//   return dp[n] = one+two;
+
+// }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,0);
+       dp[0] = dp[1] = 1;
+       for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1]+dp[i-2];
+       } 
+       return dp[n];
+    }
+};
